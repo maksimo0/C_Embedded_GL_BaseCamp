aaff2442429760618f07c59604742a6d989585c7
maksimo0 <mkdjsl@gmail.com>
2022-12-09 09:15:08 +0200

    Add subfolder and done HW1

---
+++ HW1/hello_word.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+int main() {
+    printf("Group number: 1\n");
+    printf("Full name: Maksym Salim Salamovych\n");
+    printf("Email: mkdjsl@gmail.com\n");
+    printf("GitHub repo link: https://github.com/maksimo0/C_Embedded_GL_BaseCamp\n");
+    printf("Shared GDrive folder link: https://bit.ly/3BkJz9Q\n");
+
+    return 0;
+}
