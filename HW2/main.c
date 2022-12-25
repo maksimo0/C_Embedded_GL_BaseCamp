892e3729a6486d7cae739c7085b34d6dd52c2b6d
maksimo0 <mkdjsl@gmail.com>
2022-12-25 23:47:19 +0200

    Minor fix in HW2

--- HW2/main.c
+++ HW2/main.c
@@ -4,22 +4,17 @@
 
 int main(void) {
     printf("Task 2, variant 7\n");
-    int iter = 5;
     double a, z1, z2;
-    while (iter > 0) {
-        printf("This is %d iteration\n", 6 - iter);
+    printf("Please, enter a: ");
+    while (scanf("%lf", &a) != 1) {
+        printf("Oops, you enter smth wrong!\n");
         printf("Please, enter a: ");
-        while (scanf("%lf", &a) != 1) {
-            printf("Oops, you enter smth wrong!\n");
-            printf("Please, enter a: ");
-            fflush(stdin);
-        }
-        z1 = pow(cos((3.0/8)*M_PI - (a/4)), 2) - pow(cos((11.0/8)*M_PI + (a/4)), 2)/*((1 + cos(2*((3/8)*M_PI - (a/4))))/2) - ((1 + cos(2*((11/8)*M_PI + (a/4))))/2)*/;
-        z2 = (M_SQRT2/2)*(a/2);
-        printf("Z1 = %.7f\n", z1);
-        printf("Z2 = %.7f\n", z2);
-        iter--;
+        fflush(stdin);
     }
-
+    z1 = pow(cos((3.0/8)*M_PI - (a/4)), 2) - pow(cos((11.0/8)*M_PI + (a/4)), 2)/*((1 + cos(2*((3/8)*M_PI - (a/4))))/2) - ((1 + cos(2*((11/8)*M_PI + (a/4))))/2)*/;
+    z2 = (M_SQRT2/2)*sin(a/2);
+    printf("Z1 = %.7f\n", z1);
+    printf("Z2 = %.7f\n", z2);
+    
     return 0;
 }
